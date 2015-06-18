d15f69b0f9a99e2996ff69ced0144a22b0d8651a
Vitalii <berezorudskyy@gmail.com>
2015-06-18 20:24:55 +0300

    Add dependencies

---
+++ lek7.php
@@ -0,0 +1,8 @@
+<?php
+require 'vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+echo $request->getHost();
