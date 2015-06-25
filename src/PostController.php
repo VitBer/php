0b26f0cdd5214aea7622afd8fb96c2f9dc6b91c6
Vitalii <berezorudskyy@gmail.com>
2015-06-25 20:12:39 +0300

    Install Silex

---
+++ src/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+class PostController
+{
+    public function indexAction ()
+    {
+        return 'My blog';
+    }
+    public function showAction($id)
+    {
+        return 'Post '. $id;
+    }
+}
