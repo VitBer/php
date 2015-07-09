97464b4402e95dbf316101409e636b9486ab2aca
Vitalii <berezorudskyy@gmail.com>
2015-07-09 19:56:09 +0300

    Register Doctrine provider

--- src/PostController.php
+++ src/PostController.php
@@ -2,12 +2,22 @@
 
 class PostController
 {
-    public function indexAction ()
+    public function indexAction (\Silex\Application $app)
     {
+        $conn =  $app['db'];
+        $posts = $conn->fetchAll("SELECT * FROM posts");
+
+        print_r($posts);
+
         return 'My blog';
     }
-    public function showAction($id)
+    public function showAction(\Silex\Application $app, $id)
     {
+        $conn =  $app['db'];
+        $post = $conn->fetchAssoc("SELECT * FROM posts WHERE id = ?", array((int) $id));
+
+        print_r($post);
+
         return 'Post '. $id;
     }
 }
--- web/index.php
+++ web/index.php
@@ -4,7 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../src/AppController.php';
 require_once __DIR__.'/../src/PostController.php';
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
+//Register providers
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_mysql',
+        'dbname'   => 'db',
+        'user'     => 'root',
+        'password' => 'usbw',
+        'port'     => 3307,
+    ),
+));
 
 $app->get('/', 'AppController::indexAction');
 $app->get('/blog', 'PostController::indexAction');
